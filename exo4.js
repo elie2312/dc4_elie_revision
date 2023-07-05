c001eccbec16d6da69ed0b315b2767c64bdb3be6
elie2312 <elie.bannes12@gmail.com>
2023-07-05 14:25:09 +0200

    Create exo4.js

---
+++ exo4.js
@@ -0,0 +1,5 @@
+let base = parseFloat(prompt("La base du triangle (cm):"));
+let hauteur = parseFloat(prompt("La hauteur du triangle (cm):"));
+let aire = (base * hauteur) / 2;
+
+console.log("L'aire du triangle est : " + aire + " cm²");
