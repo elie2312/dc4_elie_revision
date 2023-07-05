5cd9881e8b8f2a124717d1cfca9ba3e0fe4156ec
elie2312 <elie.bannes12@gmail.com>
2023-07-05 14:20:13 +0200

    Create exo3.js

---
+++ exo3.js
@@ -0,0 +1,5 @@
+let premierNombre = parseFloat(prompt("Votre premier nombre :"));
+let deuxiemeNombre = parseFloat(prompt("Votre deuxieme nombre :"));
+let somme = premierNombre + deuxiemeNombre;
+
+console.log("La somme des nombres est : " + somme);
