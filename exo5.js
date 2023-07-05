fd53ddb9e933515cde5dda30525161ba455222af
elie2312 <elie.bannes12@gmail.com>
2023-07-05 14:27:21 +0200

    Update exo5.js

---
+++ exo5.js
@@ -0,0 +1,9 @@
+let tableau = [1, 2, 3, 4];
+
+let somme = 0;
+
+for (let i = 0; i < tableau.length; i++) {
+  somme += tableau[i];
+}
+
+console.log("La somme des nombres dans le tableau est : " + somme);
