919c51f5e520b407a1ae62e7289141ee87481700
elie2312 <elie.bannes12@gmail.com>
2023-07-06 13:34:35 +0200

    Update exo8.js

--- niveau 1/exo8.js
+++ niveau 1/exo8.js
@@ -1,4 +1,8 @@
-let mot = parseFloat(prompt("Mot a verifier :"));
+let mot = prompt("Entrez un mot :");
+let motInverse = mot.split("").reverse().join("");
 
-
-
+if (mot.toLowerCase() === motInverse.toLowerCase()) {
+  console.log(mot + " est un palindrome.");
+} else {
+  console.log(mot + " n'est pas un palindrome.");
+}
